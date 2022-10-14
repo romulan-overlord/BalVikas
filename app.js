b9b39131997d73c89fe480ace4db85d61cbd955f
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2022-10-14 09:39:58 +0530

    finish making signup page frontend

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signin.html");
 });
 
+app.post("/", function(req, res){
+    console.log(req.body);
+    res.send("Your data has been received successfully.");
+});
+
 app.get("/signup", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/signup", function(req, res){
+    console.log(req.body);
+    res.send("Congratulations, new account made.");
 });
 
 app.listen(3030, function(err){
