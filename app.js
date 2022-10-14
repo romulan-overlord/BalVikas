3ad65f2acbd2a6416481694ebebcee9725959406
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2022-10-14 08:16:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signin.html");
+});
+
+app.listen(3000, function(err){
+    console.log("server up!");
+});
