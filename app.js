1276d5958225c0225865e76e58b4b4b85518609c
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2022-10-18 04:17:04 +0530

    add journal flavour

--- app.js
+++ app.js
@@ -3,24 +3,37 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const entries = [
+    {
+        title: "Day 1",
+        content: "I feel the sky bearing down on me"
+    },
+    {
+        title: "Day 2",
+        content: "Eternal Damnation"
+    }
+];
+
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signin.html");
+    res.render("signin");
 });
 
 app.get("/home", function(req, res){
-    res.sendFile(__dirname + "/home.html");
+    res.render("home", {entries: entries});
 });
 
 app.post("/home", function(req, res){
     console.log(req.body);
-    res.sendFile(__dirname + "/home.html");
+    res.render("home", {entries: entries});
 });
 
 app.get("/signup", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
+    res.render("signup");
 });
 
 app.post("/signup", function(req, res){
@@ -28,6 +41,10 @@ app.post("/signup", function(req, res){
     res.redirect("/home");
 });
 
-app.listen(3030, function(err){
+app.get("/compose", function(req,res){
+    res.render("compose");
+});
+
+app.listen(3000, function(err){
     console.log("server up!");
 });
---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+// let totalHeight = $(".entries-container").height();
+// let titleHeight = $(".title").height();
+// $(".content-input").height(totalHeight - titleHeight);
+
+let windowHeight = window.innerHeight;
+let titleHeight = $(".title").outerHeight();
+$(".content").height(windowHeight - titleHeight);
+
+let titleBoxHeight = $(".title-input").outerHeight();
+let contentHeight = $(".content").outerHeight();
+let settingHeight = contentHeight - 60 - titleBoxHeight;
+$(".content-input").outerHeight(settingHeight);
+
+let divWidth = $(".entries-container").width();
+
+$(".title-input").outerWidth(divWidth);
+$(".content-input").outerWidth(divWidth);
